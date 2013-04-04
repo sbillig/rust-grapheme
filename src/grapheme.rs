5725d70da4a8bd11bc48ee6ae075bcef86149ceb
Sean Billig <sean.billig@gmail.com>
2013-04-03 20:34:06 -0500

    Proof-of-concept count function

--- src/grapheme.rs
+++ src/grapheme.rs
@@ -5,12 +5,58 @@
 
 #[crate_type = "lib"];
 
+use core::str::*;
 mod char_classes;
+
+pub fn cluster_count(s: &str) -> uint {
+    use char_classes::*;
+
+    let len = s.len();
+    if len == 0 {
+        return 0;
+    }
+
+    let CharRange {ch, next} = char_range_at(s, 0);
+
+    let mut left = char_class(ch);
+    let mut pos = next;
+    let mut count = 1u;
+
+    while pos < len {
+        let CharRange {ch, next} = char_range_at(s, pos);
+        let right = char_class(ch);
+
+        if break_between(left, right) {
+            count += 1;
+        }
+
+        left = right;
+        pos = next;
+    }
+    count
+}
+
 
 #[cfg(test)]
 mod tests {
     use char_classes::*;
 
+/*
+    #[test]
+    fn test_cluster_count() {
+        fn t(s: &str, c: uint) {
+            assert!(cluster_count(s) == c);
+        }
+        t(~"", 0);
+        t(~"hello", 5);
+        t(~"a\U0001f1e6b", 3);
+        t(~"\U0001f1f7\U0001f1fa", 1);
+        t(~"\U0001f1f7\U0001f1fa\U0001f1f8", 1);
+        t(~"\U0001f1f7\U0001f1fa\U0001f1f8\U0001f1ea", 1);
+        t(~"\U0001f1f7\U0001f1fa\u200b\U0001f1f8\U0001f1ea", 3);
+        t(~"\u0020\u200d\u0646", 2);
+    }
+*/
     #[test]
     fn blah() {
         assert!(is_spacing_mark('\u09cc'));
