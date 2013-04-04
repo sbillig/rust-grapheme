492635c74ec6ed217246a26fa25a16276353bc89
Sean Billig <sean.billig@gmail.com>
2013-04-03 23:11:00 -0500

    each_cluster function

--- src/grapheme.rs
+++ src/grapheme.rs
@@ -9,37 +9,68 @@ use core::str::*;
 mod char_classes;
 
 pub fn cluster_count(s: &str) -> uint {
+    let mut c = 0u;
+    do each_cluster(s) |_| { c += 1; true }
+    c
+}
+
+pub fn each_cluster<'a>(s: &'a str, it: &fn(&'a /*'*/ str) -> bool) {
     use char_classes::*;
+    use core::str::raw::slice_bytes;
 
     let len = s.len();
     if len == 0 {
-        return 0;
+        return;
     }
 
     let CharRange {ch, next} = char_range_at(s, 0);
 
-    let mut left = char_class(ch);
-    let mut pos = next;
-    let mut count = 1u;
+    let mut left = char_class(ch),
+            pos = next,
+            startp = 0;
 
     while pos < len {
         let CharRange {ch, next} = char_range_at(s, pos);
         let right = char_class(ch);
 
         if break_between(left, right) {
-            count += 1;
+            if !it(unsafe{slice_bytes(s, startp, pos)}) { return; }
+            startp = pos;
         }
 
         left = right;
         pos = next;
     }
-    count
+    it(unsafe{slice_bytes(s, startp, len)});
 }
 
 #[cfg(test)]
 mod tests {
+    use core::str::*;
     use char_classes::*;
     use super::*;
+
+    #[test]
+    fn test_slice() {
+        use core::str::raw::slice_bytes;
+        assert!("b" == unsafe { slice_bytes("abc", 1, 2)});
+    }
+
+    #[test]
+    fn test_each_cluster() {
+        let tests = [
+            ~[~"\U0001f1f7\U0001f1fa",~"\u200b",~"\U0001f1f8\U0001f1ea"],
+            ~[~"h",~"i"]
+        ];
+        for tests.each |&chunks| {
+            let s = concat(chunks);
+            let mut i = 0u;
+            for each_cluster(s) |clus| {
+                assert!(clus == chunks[i]);
+                i += 1;
+            }
+        }
+    }
 
     #[test]
     fn test_cluster_count() {
